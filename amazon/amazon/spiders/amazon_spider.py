dffea6c210db401be21b631c7460de0f96e2ca89
archi-007 <archiribhu@gmail.com>
2020-03-31 22:37:02 +0530

    modified script to scrape multiple pages

--- amazon/amazon/spiders/amazon_spider.py
+++ amazon/amazon/spiders/amazon_spider.py
@@ -6,6 +6,7 @@ from ..items import AmazonItem
 
 class AmazonSpiderSpider(scrapy.Spider):
     name = 'amazon'
+    page_number = 2
     start_urls = ['https://www.amazon.in/s?bbn=976389031&rh=n%3A976389031%2Cp_n_publication_date%3A2684819031&dc&fst=as%3Aoff&qid=1585658099&rnid=2684818031&ref=lp_976389031_nr_p_n_publication_date_0']
 
     def parse(self, response):
@@ -27,3 +28,10 @@ class AmazonSpiderSpider(scrapy.Spider):
 
 
         yield items
+
+
+        next_page = 'https://www.amazon.in/s?i=stripbooks&bbn=976389031&rh=n%3A976389031%2Cp_n_publication_date%3A2684819031&dc&page='+str(AmazonSpiderSpider.page_number)+'&fst=as%3Aoff&qid=1585673810&rnid=2684818031&ref=sr_pg_2'
+
+        if AmazonSpiderSpider.page_number <=100:
+            AmazonSpiderSpider.page_number+=1
+            yield response.follow(next_page, callback = self.parse)
